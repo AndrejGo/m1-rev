34969986c4b68bfade4f520c421414d1c109f0b9
Andrej <andrej.gorjan@gmail.com>
2022-01-16 13:53:22 +0100

    Commit before presentation

--- btb/btb_size/analyzer.py
+++ btb/btb_size/analyzer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
+from abc import abstractproperty
 from os import EX_OK
 import subprocess
 import matplotlib.pyplot as plt
+from matplotlib import colors
 
 #plt.gcf().subplots_adjust(left=0.15)
 
@@ -60,7 +62,7 @@ class AsmGen:
 
         if interval == 1:
             return self.gen_1_2(num_branches)
-        elif interval > 1:
+        elif interval == 2:
 
             assembly = f'''
 "       adr x0, .       ; Store the current address in x0 \\n"
@@ -89,164 +91,186 @@ class AsmGen:
 
             return assembly
 
+#"off:   .quad {(2+interval) * 4}    \\n"
+#"       ldr x3, off \\n"
+
+        elif interval >= 3:
+            assembly = f'''
+"       mov x3, {interval}      \\n"
+"       add x3, x3, 2           \\n"
+"       mov x4, 4               \\n"
+"       mul x3, x3, x4           \\n"
+
+"       sub x4, x3, 4           \\n"
+
+"       adr x0, .       ; Store the current address in x0 \\n"
+"       add x0, x0, x3  \\n"
+"       br x0           ; Branch to the address in x0 \\n"
+'''.replace('\n', '')
+
+            for i in range(interval-1):
+                assembly += '''
+"       .word 10        ; Dummy instruction \\n"
+'''.replace('\n', '')
+
+            for i in range(1, num_branches):
+                assembly += f'''
+"       add x0, x0, x4                    \\n"
+"       br x0                             \\n"
+'''.replace('\n', '')
+                for i in range(interval-1):
+                    assembly += '''
+"       .word 10        ; Dummy instruction \\n"
+'''.replace('\n', '')
+
+            assembly += '''
+"       add x0, x0, 1 \\n"
+'''.replace('\n', '')
+
+            return assembly
+
         else:
             print(f'interval {interval} not supported')
             exit(0)
 
-    # gen01 generates assembly code with 1 branch per 3 instructions
-    def gen01(self, num_branches):
-        assembly = '''
-"       adr x0, .       ; Store the current address in x0 \\n"
-"       add x0, x0, 16  ; The target is 4 instruction after the above one \\n"
-"       br x0           ; Branch to the address in x0 \\n"
-"       sub x0, x0, 1   ; Dummy instruction \\n"
-'''.replace('\n', '')
+core = "FIRESTORM"
 
-        for i in range(1, num_branches):
-            assembly += '''
-"       add x0, x0, 12                    \\n"
-"       br x0                             \\n"
-"       sub x0, x0, 1 ; dummy instruction \\n"
-'''.replace('\n', '')
+def compile(params):
+    cfile = ''
+    with open('main.c.template', 'r') as f:
+        cfile = f.read()
 
-        assembly += '''
-"       add x0, x0, 1 \\n"
-'''.replace('\n', '')
+    cfile = cfile.replace('ASM_CODE', params[1])
+    cfile = cfile.replace('CLOBERED', params[2])
+    cfile = cfile.replace('LOOP_COUNT', str(params[3]))
 
-        return assembly
+    with open('main.c', 'w') as f:
+        f.write(cfile)
 
-    # gen02 generates assembly code with 2 branches per 5 instructions
-    def gen02(self, num_branches):
-        assembly = '''
-"       adr x0, .       ; Store the current address in x0 \\n"
-"       adr x1, .       ; Store the current address in x0 \\n"
+    try:
+        subprocess.run(
+            f'gcc -g main.c -D{params[0]} -o main',
+            shell=True
+        )
+    except Exception as e:
+        print('compilation of main.c failed', str(e))
 
-"       add x0, x0, 28  ; Change x0 to point to the next instruction that adds to x0 \\n"
-"       add x1, x1, 20  ; Change x1 so that it points to the next br x0 instruction  \\n"
-"       add x1, x1, 20  ;                                                            \\n"
+#x = [1, 1, 1, 1, 1, 2, 3, 4, 5, 6, 7, 7, 2]
+#y = [1, 1, 1, 1, 1, 2, 3, 4, 5, 6, 7, 7, 31]
+#plt.hist2d(x, y, bins=[[1, 2, 3, 4, 5, 6, 7], [2, 4, 8, 16, 32]])
+#plt.show()
+#exit(0)
 
-"       br x1           ; Jump to the next br x0 instruction    \\n"
-"       br x0           ; This instruction will not be executed \\n"
-'''.replace('\n', '')
+loop_count = 1000
 
-        for i in range((num_branches-2)//2):
-            assembly += '''
-"       add x0, x0, 20 ; Change x0 to point to the next add x0 instruction. \\n"
-"       add x1, x1, 10 ; Change x1 to point to the next br x1 instruction   \\n"
-"       add x1, x1, 10 ;                                                    \\n"
+branch_num_array = [16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768]
+interval_array = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
 
-"       br x1          ; Branch to the next br x0 instruction \\n"
-"       br x0          ; We come here from the previous br x1 instruction. Branch to the above add x0. \\n"
-'''.replace('\n', '')
+#branch_num_array = [16, 32]
+#interval_array = [2, 4, 8]
 
-        assembly += '''
-"       add x0, x0, 20 ; Dummy instruction, never executed. \\n"
-"       add x1, x1, 10 ; Dummy instruction, never executed. \\n"
-"       add x1, x1, 10 ; Dummy instruction, never executed. \\n"
-    
-"       br x1          ; Dummy instruction, never executed. \\n"
-"       add x0, x0, 28 ; We come here from the last br x1 instruction. Change x0 to point to the last instuction in this block. \\n"
-"       br x0          ; \\n"
+data = []
+for _ in interval_array:
+    temp = []
+    for _ in range(len(branch_num_array)):
+        temp.append(0)
+    data.append(temp)
 
-"       add x0, x0, 20 ; Dummy instruction, never executed. \\n"
+x = []
+y = []
+for a in range(len(branch_num_array)):
+    num_branches = branch_num_array[a]
+    for b in range(len(interval_array)):
+        interval = interval_array[b]
 
-"       add x0, x0, 20 ; Dummy instruction, just acts as a target for the last branch. \\n"
-'''.replace('\n', '')
+        if(num_branches * interval >= 262144):
+            data[len(interval_array) - b-1][a] = 0
+            continue
 
-        return assembly
+        print(f'{num_branches} x {interval}')
 
-    # gen03 generates assembly code with 1 branch per 2 instructions
-    def gen03(self, num_branches):
-        assembly = ''
-        start = 0
-        if num_branches % 2 == 0:
-            start = 4
-            assembly += '''
-"       adr x0, .      \\n"
-"       add x0, x0, 28 \\n"
-"       br x0          \\n"
-"       add x0, x0, 32 \\n"
-"       br x0          \\n"
-"       sub x0, x0, 8  \\n"
-"       br x0          \\n"
-"       sub x0, x0, 8  \\n"
-"       br x0          \\n"
-'''.replace('\n', '')
+        ag = AsmGen()
+        assembly = ag.gen(num_branches, interval)
+        #assembly = assembly.replace('"', '\\"')
+        #assembly = assembly.replace('\n', '\\n')
+        assembly = assembly.replace('""', '"\n"')
 
-        else:
-            start = 3
-            assembly += '''
-"       adr x0, .      \\n"
-"       add x0, x0, 20 \\n"
-"       br x0          \\n"
-"       add x0, x0, 24 \\n"
-"       br x0          \\n"
-"       sub x0, x0, 8  \\n"
-"       br x0          \\n"
-'''.replace('\n', '')
+        register_list = '"x0", "x1", "x3", "x4"'
+        #clobered = register_list.replace('"', '\\"')
 
-        for i in range(start, num_branches, 2):
-            assembly += '''
-"       add x0, x0, 24 \\n"
-"       br x0          \\n"
-"       sub x0, x0, 8  \\n"
-"       br x0          \\n"
-'''.replace('\n', '')
+        compile((core, assembly, register_list, loop_count))
 
-        assembly += '''
-"       add x0, x0, 1 \\n"
-"       add x0, x0, 1 \\n"
-"       add x0, x0, 1 \\n"
-"       add x0, x0, 1 \\n"
-'''.replace('\n', '')
+        """
+        try:
+            #print(f'gcc main.c -DASM_CODE="{assembly}" -DCLOBERED="{clobered}" -DLOOP_COUNT={loop_count} -o main')
+            subprocess.run(
+                f'gcc -g main.c -D{core} -DASM_CODE="{assembly}" -DCLOBERED="{clobered}" -DLOOP_COUNT={loop_count} -o main',
+                shell=True
+            )
+        except Exception as e:
+            print('compilation of main.c failed', str(e))
+        """
 
-        return assembly
+        average = 0
+        j = 0
+        while j < 100:
+            try:
+                result = subprocess.run(
+                    './main',
+                    stdout=subprocess.PIPE
+                )
+                core_id = int(result.stdout.decode('utf-8').split(' ')[0])
+                num = int(result.stdout.decode('utf-8').split(' ')[1])
+                time = int(result.stdout.decode('utf-8').split(' ')[2][:-1])
+                if num < 1000 * (num_branches+10):
+                    if core == "ICESTORM":
+                        if core_id == 1:
+                            average += num
+                            j += 1
+                    else:
+                        if core_id == 0:
+                            average += num
+                            j += 1
+            except Exception as e:
+                print(f'({num_branches} {interval}) running ./main returned an error: {str(e)}\n{result.stdout.decode()}')
 
-    # This is a bad chunk of assembly, since all branches have the same target
-    # gen04 generates assembly code with 1 branch per 1 instruction
-    def gen04(self, num_branches):
-        assembly = '''
-"       adr x0, .       \\n"
-"       adr x1, .       \\n"
+        average //= 100
+        data[len(interval_array) - b-1][a] = average
+        #for i in range(average):
+        #    x.append(num_branches)
+        #    y.append(interval)
 
-"       add x0, x0, 16  \\n"
-"       add x1, x1, 20  \\n"
- 
-"       add x1, x1, 4  ; x0 always points here \\n"
-"       br x1                                 \\n"
+#branch_num_array.append(branch_num_array[-1] * 2)
+#interval_array.append(interval_array[-1] * 2)
 
-"       add x0, x0, 1  ; dummy instruction, never executed \\n"
-'''.replace('\n', '')
-
-        for i in range(num_branches):
-            assembly += '''
-"       br x0          \\n"
-'''.replace('\n', '')
-
-        assembly += '''
-"       add x0, x0, 1       \\n"
-'''.replace('\n', '')
-
-        return assembly
-
-
-
-core = "ICESTORM"
+print(data)
+#plt.xscale('log', base=2)
+#plt.yscale('log', base=2)
+#plt.hist2d(x, y, bins=[branch_num_array, interval_array], cmap=plt.cm.jet)
+plt.imshow(data, cmap='magma', norm=colors.LogNorm())
+#plt.imshow(data, cmap='cividis')
+#plt.colorbar()
+plt.colorbar(pad=0.03)
+plt.title('Average number of mispredictions', fontsize=20)
+plt.xlabel('log₂(number of branches) - 4', fontsize=18)
+plt.ylabel('Address interval', fontsize=18)
+plt.xticks(range(len(branch_num_array)), fontsize=16)
+plt.yticks(range(len(interval_array)), interval_array[::-1], fontsize=16)
+plt.show()
+exit(0)
 
 loop_count = 1000
 
 x = list(map(lambda x : x + 450, range(2)))
 t = []
 y = []
+
 for num_branches in x:
 
     print(num_branches)
 
     ag = AsmGen()
     assembly = ag.gen(10, 3)
-
-    print(assembly.replace('""', '\n'))
-    exit(0)
 
     assembly = assembly.replace('"', '\\"')
     assembly = assembly.replace('\n', '\\n')
--- loop_predictor/loop_predictor_2_avg/analyzer.py
+++ loop_predictor/loop_predictor_2_avg/analyzer.py
@@ -1,62 +1,88 @@
 #!/usr/bin/python3
 
 import subprocess
+import matplotlib
 import matplotlib.pyplot as plt
 
-core = "FIRESTORM"
+matplotlib.rc('xtick', labelsize=16) 
+matplotlib.rc('ytick', labelsize=16)
+fig, ax = plt.subplots(3, 1, sharex=True)
+fig.suptitle("Average number of mispredictions", fontsize=20)
+fig.subplots_adjust(bottom=0.15, left=0.15, top=0.8, hspace=0.8)
+#fig.tight_layout()
 
-start = 95
-upper_limit = 105
-results = []
+start = 1
+upper_limit = 110
 
-# For different iteration numbers of the internal loop
-for inner in range(start, upper_limit+1):
-    print(inner)
-    # Compile the program with this number
-    try:
-        subprocess.check_output(f'clang -g -O0 -DINNER_COUNT="{inner}" main_do_while.c -o main', shell=True)
-                
-    except Exception as e:
-        print("Compilation failed")
-        exit()
+core = 'FIRESTORM'
 
-    # Run the program 100 times and calculate the average number of mispredictions
-    avg = 0
-    num_repetitions = 100
-    i = 0
-    while(i < num_repetitions):
+for loop_type in ['for', 'while', 'do_while']:
+
+    results = []
+
+    # For different iteration numbers of the internal loop
+    for inner in range(start, upper_limit+1):
+
+        print(inner)
+
+        # Compile the program with this number
         try:
-            result = subprocess.check_output(f'./main', shell=True).decode('utf-8')
-            # Check if we got an illegal instruction error
-            if "illegal" not in result:
-                coreid = int(result.split(' ')[0])
-                val = int(result.split(' ')[1][:-1])
-
-                if val >= 1000 * (2 + inner):
-                    print(val)
-                    continue
-
-                if core == "ICESTORM":
-                    if coreid == 0:
-                        avg += val
-                        i += 1
-                else:
-                    if coreid == 1:
-                        avg += val
-                        i += 1
-
+            subprocess.check_output(f'clang -g -O0 -DINNER_COUNT="{inner}" main_{loop_type}.c -o main', shell=True)
         except Exception as e:
-            print(str(e))
+            print("Compilation failed")
+            exit()
 
-    avg //= num_repetitions
-    results.append(avg)
+        # Run the program 100 times and calculate the average number of mispredictions
+        avg = 0
+        num_repetitions = 10
+        i = 0
+        while(i < num_repetitions):
+            try:
+                result = subprocess.check_output(f'./main', shell=True).decode('utf-8')
+                # Check if we got an illegal instruction error
+                if "illegal" not in result:
+                    coreid = int(result.split(' ')[0])
+                    val = int(result.split(' ')[1][:-1])
+                    if val >= 1000 * (2 + inner):
+                        print(val)
+                        continue
+                    if core == "ICESTORM":
+                        if coreid == 0:
+                            avg += val
+                            i += 1
+                    else:
+                        if coreid == 1:
+                            avg += val
+                            i += 1
+            except Exception as e:
+                print(str(e))
+        avg //= num_repetitions
+        results.append(avg)
 
-plt.subplots_adjust(bottom=0.15, left=0.14, top=0.90)
-plt.title("Average number of mispredictions", fontsize=20)
-plt.xlabel("Number of iterations of the\nnested branch.", fontsize=18)
-plt.xticks(fontsize=16)
-plt.yticks(fontsize=16)
-plt.plot(range(start, upper_limit+1), results, color='#ff0000')
+    if loop_type == 'for':
+        ax[0].plot(range(start, upper_limit+1), results, color='#ff0000', linewidth=3)
+    elif loop_type == 'while':
+        ax[1].plot(range(start, upper_limit+1), results, color='#ff0000', linewidth=3)
+    else:
+        ax[2].plot(range(start, upper_limit+1), results, color='#ff0000', linewidth=3)
+
+dark = '#0000ff'
+light = '#5485ff'
+if core == "FIRESTORM":
+    dark = '#ff0000'
+    light = '#ff5454'
+
+ax[0].set_title('for loops', size=18, style='italic')
+ax[1].set_title('while loops', size=18, style='italic')
+ax[2].set_title('do-while loops', size=18, style='italic')
+ax[2].set_xlabel("COUNT", fontsize=18)
+
+#plt.subplots_adjust(bottom=0.15, left=0.14, top=0.82)
+#plt.plot(range(start, upper_limit+1), results, color=dark, linewidth=3)
+#plt.title(f"Average number of mispredictions,\nfor loops.", fontsize=20)
+#plt.xlabel("COUNT", fontsize=18)
+#plt.xticks(fontsize=16)
+#plt.yticks(fontsize=16)
 plt.show()
 
 """
